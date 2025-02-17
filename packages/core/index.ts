5466bf771a84a78d4964243d0d0f34d53bf1b6e9
shanto4354 <mdshanto2520@gmail.com>
2025-02-17 09:22:30 +0600

    Delete packages directory

--- packages/core/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './xaven-context';
-export * from './xaven-factory';
--- packages/core/interfaces/module-definition.ts
+++
@@ -1,8 +0,0 @@
-import { DynamicModule, ForwardReference } from '@/common';
-import { Type } from '@/common/interfaces';
-
-export type ModuleDefinition =
-  | ForwardReference
-  | Type<unknown>
-  | DynamicModule
-  | Promise<DynamicModule>;
